416fb30bcac5e4751984ec60e2591a84fcd76e94
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-08-19 15:59:15 -0500

    commit

---
+++ loopchallenges.js
@@ -0,0 +1,35 @@
+for(var i=1; i<21; i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+console.log("*****************************");
+
+for(var i=100; i>-1; i--){
+    if(i%3==0){
+    console.log(i); 
+    }
+}
+
+console.log("*****************************");
+
+for(var i=4; i>-4; i-=1.5){
+    console.log(i);
+}
+
+console.log("*****************************");
+
+var sum=0;
+for(var i=1; i<101; i++){
+    sum = sum+i;
+}
+console.log(sum);
+
+console.log("*****************************");
+
+var product = 1;
+for(var i=1; i<13; i++){
+    product = product*i;
+}
+console.log(product);
